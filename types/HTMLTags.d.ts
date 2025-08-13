00b0f062737788f3f2d61cd70961bdb1c896a027
ЯRаСлав <support@yaro.page>
2025-08-13 23:25:40 +0300

    Types added.

---
+++ types/HTMLTags.d.ts
@@ -0,0 +1,30 @@
+export default HTMLTags;
+declare class HTMLTags {
+    $default: string;
+    $nonEmptyTags: string[];
+    $attrTrue: string;
+    /**
+     * Used in xml2nano to conver elements such as table > tbody > tr => table > tr
+     * if such child is a single, so only tbody, if there are tbody with thead or
+     * tfoot it won't remove the layer.
+     */
+    $singleChild: string[];
+    /**
+     * Transform tag into nano with the attributes right from its definition.
+     * @example
+     *   { '.': 'class' } => div.d-flex.flex-wrap => <div class="d-flex flex-wrap">
+     *   { '#': 'id } => div#main => <div id="main">
+     *   { '.': 'class', '#': 'id } => div.d-flex#main.flex-wrap => <div class="d-flex flex-wrap" id="main">
+     */
+    $tagAttrs: {
+        '.': string;
+        '#': string;
+    };
+    ul: string;
+    ol: string;
+    dl: string;
+    table: string;
+    tr: string;
+    select: string;
+    $selfClosed(tag: any): string;
+}
---
+++ types/Transformer.d.ts
@@ -0,0 +1,8 @@
+export default HTMLTransformer;
+/**
+ * Class representing an HTML transformer.
+ * Encodes nano objects to HTML format, and decodes HTML strings to nano objects.
+ */
+declare class HTMLTransformer extends XMLTransformer {
+}
+import XMLTransformer from "@nan0web/xml";
